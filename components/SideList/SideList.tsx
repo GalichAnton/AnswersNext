c98a3909d182ae65d25460c749d2a9e83687a090
Anton Galich <inthesky133@gmail.com>
2022-04-25 13:34:43 +0300

    add active state to links

--- components/SideList/SideList.tsx
+++ components/SideList/SideList.tsx
@@ -3,14 +3,22 @@ import React from "react";
 import { SideListProps } from "./SideListProps";
 import styles from "./SideList.module.css";
 import cn from "classnames";
+import { useRouter } from "next/router";
 const SideList = ({ answer, className, ...props }: SideListProps) => {
+  const router = useRouter();
   return (
     <div className={cn(className, styles.sideList)} {...props}>
       <h2 className={styles.title}>{answer?.title}</h2>
       <ul className={styles.list}>
-        {" "}
         {answer?.items.map((item, i) => (
-          <li key={item.id} className={styles.link}>
+          <li
+            key={item.id}
+            className={cn(styles.link, {
+              [styles.activeLink]:
+                router.asPath === `/answers/${answer.name}/${item.id}` ||
+                router.asPath === `/tasks/${answer.name}/${item.id}`,
+            })}
+          >
             <Link
               href={
                 answer.name.includes("task")
