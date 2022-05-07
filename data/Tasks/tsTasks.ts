6f1da9ec94ebfe6acabec2ba007d67173776a7a1
Anton Galich <inthesky133@gmail.com>
2022-05-07 07:40:52 +0300

    refactor styles

--- data/Tasks/tsTasks.ts
+++ data/Tasks/tsTasks.ts
@@ -8,7 +8,7 @@ export const tsTasks: ITask[] = [
     descr: "Написать свою реализацию основных методов массивов(с типизацией)",
     video: "ugtnxm18M8o",
     codepenUrl:
-      "https://codesandbox.io/embed/custom-array-methods-ts-3hkbmp?fontsize=14&hidenavigation=1&theme=dark",
+      "https://codesandbox.io/embed/custom-array-methods-ts-3hkbmp?fontsize=14&hidenavigation=1&theme=dark&view=editor",
   },
   {
     id: "2",
@@ -17,6 +17,6 @@ export const tsTasks: ITask[] = [
     descr: "С помощью reduce реализовать map, filter, группировку",
     video: "iBGUyPwm_dM",
     codepenUrl:
-      "https://codesandbox.io/embed/reduce-as-filter-map-group-by-5g5z2i?fontsize=14&hidenavigation=1&theme=dark",
+      "https://codesandbox.io/embed/reduce-as-filter-map-group-by-5g5z2i?expanddevtools=1&fontsize=14&hidenavigation=1&theme=dark&view=editor",
   },
 ];
