9036aba392277369612490697153fd2d8b8f9d7d
aagalich <aagalich@avito.ru>
2024-10-07 15:30:25 +0300

    test amplitude

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import * as amplitude from '@amplitude/analytics-browser';
 
 import Layout from "../layout/Layout";
 
+amplitude.init('2082ab2a4b7ab2b8cdd162f713b2798', {"autocapture":true});
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+        TEST
       <Component {...pageProps} />
     </Layout>
   );
